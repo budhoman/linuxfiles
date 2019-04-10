832cdee92f35ac684f4429232bae67fff34c7103
Paul R <42287355+ftlol@users.noreply.github.com>
2019-04-10 18:34:29 -0500

    Update nv.sh

--- nv.sh
+++ nv.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }"
+nvidia-settings --assign CurrentMetaMode="HDMI-0: nvidia-auto-select +1920+0 {ForceCompositionPipeline=On"
